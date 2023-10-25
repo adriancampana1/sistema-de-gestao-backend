97b3b386ad8ec6016bcc34d892d8c4d419ffba06
Adrian Campana <adrianphcampana12@gmail.com>
2023-10-25 17:35:43 -0300

    feat: pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'app',
-            script: './app.js',
+            script: './src/server.js',
             instances: 'max',
             env: {
                 NODE_ENV: 'development',
